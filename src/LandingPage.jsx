b655b8c88df3a7e5b82723bc71a4ea5244056523
Quirites-ihemi <nathalie.kakpo@interieur.gouv.fr>
2026-09-25 15:06:29 +0200

    Add files via upload

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -17,9 +17,9 @@ export default function LandingPage({ onEnter }) {
 
   return (
     <div className="qlab-radar-landing">
-      <div className="qlab-radar-landing__background" aria-hidden="true">
+      <div className="qlab-radar-landing__visual" aria-hidden="true">
         <img src={radarVisual} alt="" />
-        <div className="qlab-radar-landing__background-mask" />
+        <div className="qlab-radar-landing__visual-fade" />
         <div className="qlab-radar-landing__radar-sweep" />
         <div className="qlab-radar-landing__pulse qlab-radar-landing__pulse--one" />
         <div className="qlab-radar-landing__pulse qlab-radar-landing__pulse--two" />
